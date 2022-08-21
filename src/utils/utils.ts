c8e0fab26ef65fc553d0ddefc32f9c4743f98f95
chyu <1163706779@qq.com>
2022-08-21 23:07:11 +0800

    feat: 新增访客数据展示

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -20,8 +20,8 @@ export const throttle = (cb: Function, time: number): Function=>{
             }, time)
     }
 }
-export const formatDate = function(time: number | string | Date){
-    var date = new Date(time);
+export const formatDate = function($date: number | string | Date, time: boolean = true){
+    var date = new Date($date);
  
     var year = date.getFullYear(),
         month = date.getMonth()+1,//月份是从0开始的
@@ -29,13 +29,18 @@ export const formatDate = function(time: number | string | Date){
         hour = date.getHours(),
         min = date.getMinutes(),
         sec = date.getSeconds();
-    var newTime = year + '-' +
-                (month < 10? '0' + month : month) + '-' +
-                (day < 10? '0' + day : day) + ' ' +
-                (hour < 10? '0' + hour : hour) + ':' +
-                (min < 10? '0' + min : min) + ':' +
-                (sec < 10? '0' + sec : sec);
- 
+    var newTime
+    if(time)
+        newTime = year + '-' +
+            (month < 10? '0' + month : month) + '-' +
+            (day < 10? '0' + day : day) + ' ' +
+            (hour < 10? '0' + hour : hour) + ':' +
+            (min < 10? '0' + min : min) + ':' +
+            (sec < 10? '0' + sec : sec);
+    else
+        newTime = year + '-' +
+            (month < 10? '0' + month : month) + '-' +
+            (day < 10? '0' + day : day)
     return newTime;         
 }
 export const useVisibleRef = ()=>{
@@ -64,4 +69,23 @@ export const useScrollTotop = ()=>{
 	useEffect(()=>{
         window.scrollTo(0, 0)
     }, [])
+}
+const addOneDay = function(currentDay: string | number | Date) {
+    let d: Date = new Date(currentDay);
+    d.setTime(d.getTime() + 1000 * 60 * 60 * 24);
+    return d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+}
+export const getEveryDay = function(startDate: string | number | Date, endData: string | number | Date) {
+    let dateList = [addOneDay(new Date(startDate))];
+    let dayCount: number = (new Date(endData).getTime() - new Date(startDate).getTime()) / (1000 * 60 * 60 * 24);
+    for (let i = 0; i < dayCount-1; i++) {
+        let currentDay = addOneDay(dateList[i]);
+        currentDay = formatDate(currentDay, false);
+        dateList.push(currentDay);
+    }
+    return dateList;
+}
+export const randomNumber = (min=0, max=1)=>{
+    let {floor, random} = Math
+    return floor(random()*(max-min)+min)
 }
